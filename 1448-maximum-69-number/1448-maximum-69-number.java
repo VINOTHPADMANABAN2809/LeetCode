3cb8860573409358c4c94bbfce8e6bc01c92b729
Vinoth kumar MP <162871515+VINOTHPADMANABAN2809@users.noreply.github.com>
2025-04-21 11:34:26 +0530

    Time: 0 ms (100%), Space: 40.6 MB (45.67%) - LeetHub

---
+++ 1448-maximum-69-number/1448-maximum-69-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximum69Number (int num) {
+        if(num==9999) return num;
+        String s = String.valueOf(num);
+        char[] ch = s.toCharArray();
+        for(int i=0;i<ch.length;i++){
+            if(ch[i]=='6') {
+                ch[i] = '9';
+                break;
+            }
+        }
+        return Integer.parseInt(new String (ch));
+
+    }
+}
